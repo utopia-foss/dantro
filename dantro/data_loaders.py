88455e8e4d3dc618ae108dc680e31fa2a8ca9503
Yunus Sevinchan <YunusSevinchan@gmail.com>
2018-03-25 13:22:05 +0200

    Improve data loaders module docstring

--- dantro/data_loaders.py
+++ dantro/data_loaders.py
@@ -2,14 +2,19 @@
 
 All these mixin classes should follow the pattern:
 class LoadernameLoaderMixin:
+
+    @add_loader(TargetCls=TheTargetContainerClass)
     def _load_loadername(filepath: str, *, TargetCls):
         # ...
         return TargetCls(...)
-    _load_loadername.TargetCls = TheTargetContainerClass
 
 Each `_load_loadername` method gets supplied with path to a file and the
 `TargetCls` argument, which can be called to create an object of the correct
 type and name.
+By default, and to decouple the loader from the container, it should be
+considered to be a staticmethod; in other words: the first positional argument
+should _not_ be `self`!
+If `self` is required, `omit_self=False` may be given to the decorator.
 """
 
 import warnings
