cffd87f57f4ad89b85cc179075fb454ffa17a4ea
Yunus Sevinchan <YunusSevinchan@gmail.com>
2018-07-13 11:52:14 +0200

    Bump version to 0.1.0

--- dantro/__init__.py
+++ dantro/__init__.py
@@ -9,7 +9,7 @@ logging.basicConfig(format="%(levelname)-7s %(module)-12s %(message)s",
 log = logging.getLogger(__name__)
 
 # Define version variable
-__version__ = '0.1.0-rc.5'
+__version__ = '0.1.0'
 # NOTE This should always be the same as in setup.py
 
 # TODO consider making some classes available here
