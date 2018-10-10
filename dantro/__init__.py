1a3872696f2d5ec13d5a12ab75935a1f8e075e9a
Yunus Sevinchan <YunusSevinchan@gmail.com>
2018-10-10 13:59:29 +0200

    Add changelog and bump version to 0.4.0-pre.3

--- dantro/__init__.py
+++ dantro/__init__.py
@@ -9,7 +9,7 @@ logging.basicConfig(format="%(levelname)-7s %(module)-12s %(message)s",
 log = logging.getLogger(__name__)
 
 # Define version variable
-__version__ = '0.4.0-pre.2'
+__version__ = '0.4.0-pre.3'
 # NOTE This should always be the same as in setup.py
 
 # TODO consider making some classes available here
