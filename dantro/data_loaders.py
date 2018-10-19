e804b249a03687a61971f96a4a738e4d40696a50
Yunus Sevinchan <yunussevinchan@gmail.com>
2018-10-19 19:30:26 +0200

    Let yaml-loader also load into ObjectContainer

--- dantro/data_loaders.py
+++ dantro/data_loaders.py
@@ -81,9 +81,28 @@ class YamlLoaderMixin:
 
         # Populate the target container with the data
         return TargetCls(data=d, attrs=dict(filepath=filepath))
+    
+    @add_loader(TargetCls=ObjectContainer)
+    def _load_yaml_to_object(filepath: str, *, TargetCls: type) -> ObjectContainer:
+        """Load a yaml file from the given path and creates a container to
+        store that data in.
+        
+        Args:
+            filepath (str): Where to load the yaml file from
+            TargetCls (type): The class constructor
+        
+        Returns:
+            ObjectContainer: The loaded yaml file as a container
+        """
+        # Load the dict
+        d = tools.load_yml(filepath)
+
+        # Populate the target container with the data
+        return TargetCls(data=d, attrs=dict(filepath=filepath))
 
     # Also make available under `yml`
     _load_yml = _load_yaml
+    _load_yml_to_object = _load_yaml_to_object
 
 # -----------------------------------------------------------------------------
 
