731bb563648bdd1bee5b7eef3291f8d7fb478d0b
Yunus Sevinchan <yunussevinchan@gmail.com>
2020-05-12 13:39:36 +0200

    Add `generate_lambda` operation

--- dantro/utils/data_ops.py
+++ dantro/utils/data_ops.py
@@ -1,5 +1,7 @@
 """This module implements data processing operations for dantro objects"""
 
+import re
+import math
 import logging
 import operator
 from importlib import import_module as _import_module
@@ -8,6 +10,7 @@ from typing import Callable, Any, Sequence, Union, Tuple, Set
 
 import numpy as np
 import xarray as xr
+import scipy
 
 import sympy as sym
 from sympy.parsing.sympy_parser import (parse_expr as _parse_expr,
@@ -223,6 +226,85 @@ def expression(expr: str, *,
             f"The expression evaluated to:\n\n    {res}\n"
         ) from exc
 
+def generate_lambda(expr: str) -> Callable:
+    """Generates a lambda from a string. This is useful when working with
+    callables in other operations.
+
+    The ``expr`` argument needs to be a valid Python ``lambda`` expression, see
+    `here <docs.python.org/3/tutorial/controlflow.html#lambda-expressions>`_.
+
+    Inside the lambda body, the following names are available for use:
+
+        * A large part of the ``builtins`` module
+        * Every name from the Python ``math`` module, e.g. ``sin``, ``cos``, …
+        * These modules (and their long form): ``np``, ``xr``, ``scipy``
+
+    Internally, this uses ``eval`` but imposes the following restrictions:
+
+        * The following strings may *not* appear in ``expr``: ``;``, ``__``.
+        * There can be no nested ``lambda``, i.e. the only allowed lambda
+          string is that in the beginning of ``expr``.
+        * The dangerous parts from the ``builtins`` module are *not* available.
+
+    Args:
+        expr (str): The expression string to evaluate into a lambda.
+
+    Returns:
+        Callable: The generated Callable.
+
+    Raises:
+        SyntaxError: Upon failed evaluation of the given expression, invalid
+            expression pattern, or disallowed strings in the lambda body.
+    """
+    ALLOWED_BUILTINS = (
+        "abs", "all", "any", "callable", "chr", "divmod", "format", "hash",
+        "hex", "id", "isinstance", "issubclass", "iter", "len", "max", "min",
+        "next", "oct", "ord", "print", "repr", "round", "sorted", "sum",
+        "None", "Ellipsis", "False", "True", "bool", "bytes", "bytearray",
+        "complex", "dict", "enumerate", "filter", "float", "frozenset", "int",
+        "list", "map", "range", "reversed", "set", "slice", "str", "tuple",
+        "type", "zip", "open"
+    )
+    DISALLOWED_STRINGS = ("lambda", ";", "__")
+    LAMBDA_PATTERN = r"^\s*lambda\s[\w\s\,\*]+\:(.+)$"
+    #                   arguments definitions  : lambda body (capture group)
+    # See also:  https://regex101.com/r/OmI8NY/2/
+
+    # Check if the given expression matches this pattern
+    pattern = re.compile(LAMBDA_PATTERN)
+    match = pattern.match(expr)
+    if match is None:
+        raise SyntaxError(f"The given expression '{expr}' was not a valid "
+                          "lambda expression!")
+
+    # Check if the lambda body contains disallowed strings
+    lambda_body = match[1]
+    if any([bad_str in lambda_body for bad_str in DISALLOWED_STRINGS]):
+        raise SyntaxError("Encountered one or more disallowed strings in the "
+                          f"body ('{lambda_body}') of the given lambda "
+                          f"expression ('{expr}'). Make sure none of the "
+                          f"following strings appears there: "
+                          f"{DISALLOWED_STRINGS}")
+
+    # Ok, sanitized enough now. Prepare the globals dict, restricting access to
+    # a subset of builtins and only allowing commonly used math functionality
+    _g = dict(
+        __builtins__={n: f for n, f in __builtins__.items()
+                      if n in ALLOWED_BUILTINS},
+        math=math, scipy=scipy, numpy=np, xarray=xr, np=np, xr=xr,
+        **{n: f for n, f in math.__dict__.items() if not n.startswith("_")},
+    )
+
+    # Try evaluation now (with empty locals)
+    try:
+        f = eval(expr, _g, {})
+
+    except Exception as exc:
+        raise SyntaxError(f"Failed generating lambda object from expression "
+                          f"'{expr}'! Got a {exc.__class__.__name__}: {exc}"
+                          ) from exc
+
+    return f
 
 # .............................................................................
 # numpy and xarray operations
@@ -439,6 +521,10 @@ _OPERATIONS = KeyOrderedDict({
     'call':         lambda c, *a, **k: c(*a, **k),
     'import_and_call':
         lambda m, n, *a, **k: import_module_or_object(m, n)(*a, **k),
+
+    # Defining lambdas
+    'lambda':       generate_lambda,
+    'call_lambda':  lambda e, *a, **k: generate_lambda(e)(*a, **k),
 
     # Some commonly used types
     'list':         list,
