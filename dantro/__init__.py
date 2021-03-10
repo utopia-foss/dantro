b53f52f615e4d4d4078c75acbee6cbcc18bab825
Benjamin Herdeanu <b.herdeanu@gmail.com>
2021-03-10 16:48:59 +0100

    Bump version and add a changelog entry

--- dantro/__init__.py
+++ dantro/__init__.py
@@ -1,7 +1,7 @@
 """dantro lets you load and manipulate hierarchically organized data"""
 
 # Package version
-__version__ = "0.16.1a1"
+__version__ = "0.16.1a2"
 
 # Configure the logging module for the whole package here by importing the
 # dantro-specific logging module
