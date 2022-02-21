32b0df8c356d4c4ea360bb8ddf8ff1bd8dedc184
Yunus Sevinchan <yunussevinchan@gmail.com>
2022-02-21 15:35:48 +0100

    Add changelog entry and bump version

--- dantro/__init__.py
+++ dantro/__init__.py
@@ -1,14 +1,22 @@
-"""dantro lets you load and manipulate hierarchically organized data"""
+"""dantro provides a uniform interface for hierarchically structured and
+semantically heterogeneous data.
+It is built around three main features:
 
-# Package version
-__version__ = "0.18.0a2"
+    - **data handling**: loading heterogeneous data into a tree-like data structure, providing a uniform interface to it
+    - **data transformation**: performing arbitrary operations on the data, if necessary using lazy evaluation
+    - **data visualization**: creating a visual representation of the processed data
 
-# Configure the logging module for the whole package here by importing the
-# dantro-specific logging module
+Together, these stages constitute a **data processing pipeline**:
+an automated sequence of predefined, configurable operations.
+"""
+
+__version__ = "0.18.0a3"
+"""Package version"""
+
 from .logging import getLogger
 
 log = getLogger(__name__)
 
-# Make manager classes available
+# -- Most important dantro classes --------------------------------------------
 from .data_mngr import DataManager
 from .plot_mngr import PlotManager
