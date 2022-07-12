8f1db778354044a86a1af81dbb56415958b1b127
Jeremias Traub <jtraub@iup.uni-heidelberg.de>
2022-07-12 09:26:08 +0200

    Migrate ColorManager from utopya

--- dantro/plot/utils/__init__.py
+++ dantro/plot/utils/__init__.py
@@ -1,4 +1,5 @@
 """Subpackage that organizes general plotting utilities."""
 
+from .color_mngr import ColorManager
 from .mpl import figure_leak_prevention
 from .plot_func import PlotFuncResolver, is_plot_func
