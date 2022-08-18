d13e8b7ad8f82e85da82a331c189fdceba794a23
Yunus Sevinchan <yunussevinchan@gmail.com>
2022-08-18 12:51:51 +0200

    Do not expose logger in dantro.__init__

--- dantro/__init__.py
+++ dantro/__init__.py
@@ -2,9 +2,12 @@
 and semantically heterogeneous data.
 It is built around three main features:
 
-- **data handling**: loading heterogeneous data into a tree-like data structure, providing a uniform interface to it
-- **data transformation**: performing arbitrary operations on the data, if necessary using lazy evaluation
-- **data visualization**: creating a visual representation of the processed data
+- **data handling**: loading heterogeneous data into a tree-like data
+  structure, providing a uniform interface to it
+- **data transformation**: performing arbitrary operations on the data, if
+  necessary using lazy evaluation
+- **data visualization**: creating a visual representation of the processed
+  data
 
 Together, these stages constitute a **data processing pipeline**:
 an automated sequence of predefined, configurable operations.
@@ -15,9 +18,10 @@ See :ref:`the user manual <welcome>` for more information.
 __version__ = "0.18.3b0"
 """Package version"""
 
-from .logging import getLogger
+# Set up the root logger such that the logging configuration is applied
+from .logging import getLogger as _getLogger
 
-log = getLogger(__name__)
+_log = _getLogger(__name__)
 
 # -- Most important dantro classes --------------------------------------------
 from .data_mngr import DataManager
