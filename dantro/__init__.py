95173c74bc9501c3a2a9c61fc4fde56bee4dfdd2
Yunus Sevinchan <yunussevinchan@gmail.com>
2023-10-30 17:33:48 +0100

    Add changelog entry and bump version

--- dantro/__init__.py
+++ dantro/__init__.py
@@ -15,7 +15,7 @@ an automated sequence of predefined, configurable operations.
 See :ref:`the user manual <welcome>` for more information.
 """
 
-__version__ = "0.19.4b0"
+__version__ = "0.19.4"
 """Package version"""
 
 # Set up the root logger such that the logging configuration is applied
