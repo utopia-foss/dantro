1d94df9c1293d694f5fccded6baf9384357f7d80
Yunus Sevinchan <blsqr@posteo.com>
2026-01-18 20:48:51 +0100

    Add changelog entry, bump version and year (2026 🎉)

--- doc/conf.py
+++ doc/conf.py
@@ -43,7 +43,7 @@ def find_version(*file_paths) -> str:
 # -- Project information -----------------------------------------------------
 
 project = "dantro"
-copyright = "2018 — 2025, dantro developers"
+copyright = "2018 — 2026, dantro developers"
 author = "dantro developers"
 
 # The short X.Y version
